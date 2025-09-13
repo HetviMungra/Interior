c6e854c175664b0015abb7972b4869458a291468
HetviMungra <hetvimungra1367@gmail.com>
2025-09-13 12:31:37 +0530

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,29 +13,29 @@ export default function Hero() {
       />
 
       {/* Text Overlay */}
-      <div className="absolute inset-0 flex flex-col justify-start lg:py-25 py-3 items-center text-center z-10 px-4">
+      <div className="absolute inset-0 flex flex-col justify-start md:justify-center lg:justify-start lg:py-30 items-center text-center z-10 px-4 pt-12 md:pt-0">
         <motion.h1
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-          className="text-3xl sm:text-5xl lg:text-7xl md:text-6xl  font-light lg:tracking-widest text-[#55391e]"
+          className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-light tracking-wide lg:tracking-widest text-[#704c2a]"
         >
-          LIVE WITH
+          LIVE  WITH
         </motion.h1>
 
         <motion.h2
           initial={{ opacity: 0, y: 60 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.2 }}
-          className="text-4xl sm:text-6xl lg:text-9xl  md:text-8xl font-light lg:tracking-widest text-[#55391e] mt-2"
+          className="text-4xl sm:text-4xl md:text-8xl lg:text-9xl font-light tracking-wide lg:tracking-widest text-[#704c2a] "
         >
           COMFORT
         </motion.h2>
       </div>
 
       {/* Bottom Right Scroll Message */}
-      <div className="absolute bottom-4 right-4 mb-3 md:mb-0 md:bottom-6 md:right-10 bg-white/90 px-6 py-2 rounded-tl-3xl rounded-br-3xl shadow-md">
-        <p className="text-gray-700 text-xs sm:text-sm md:text-lg font-medium">
+      <div className="absolute  right-4 md:bottom-8 md:right-10 bg-white/90 px-6 py-2 rounded-tl-3xl rounded-br-3xl shadow-md ">
+        <p className="text-gray-700 text-xs  sm:text-sm md:text-lg font-medium">
           Scroll To Explore
         </p>
       </div>
